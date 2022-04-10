b1b716baa1f38962c3fcb099efaa99fa7d2485ab
star-hengxing <hengxings783@gmail.com>
2022-04-10 18:59:45 +0800

    fix bsdf bug

--- src/Core/BSDF.cpp
+++ src/Core/BSDF.cpp
@@ -17,11 +17,11 @@ Vector3f BSDF::to_world(const Vector3f& w) const
 
 BSDF::BSDF(const hit_record& record, BxDF* bxdf)
 {
-    auto [t, b] = local_coordinate_system(n);
+    this->bxdf = bxdf;
+    this->n = record.n;
+    const auto [t, b] = local_coordinate_system(n);
     this->t = t;
     this->b = b;
-    this->n = record.n;
-    this->bxdf = bxdf;
 }
 
 Spectrum BSDF::f(const Vector3f& wi, const Vector3f& wo) const
@@ -33,6 +33,7 @@ std::tuple<Spectrum, Vector3f, f32>
 BSDF::sample_f(const Vector3f& wi, const Point2f& p) const
 {
     const auto local_wi = to_local(wi);
-    const auto [f, wo, pdf] = bxdf->sample_f(local_wi, p);
-    return {f, to_world(wo), pdf};
+    auto [f, wo, pdf] = bxdf->sample_f(local_wi, p);
+    wo = to_world(wo);
+    return {f, wo, pdf};
 }
